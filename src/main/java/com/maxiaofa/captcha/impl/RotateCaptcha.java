e14847ddfb327bb992f57422d864608d59ad8a27
马小法 <mxw0204@gmail.com>
2021-12-05 15:36:12 +0800

    修复生成图像大小不一

--- src/main/java/com/maxiaofa/captcha/impl/RotateCaptcha.java
+++ src/main/java/com/maxiaofa/captcha/impl/RotateCaptcha.java
@@ -33,18 +33,15 @@ public class RotateCaptcha extends Configurable implements Producer {
         // 与偏移角度相加
         angle = (angle + getConfig().getRotationAngleOffset());
 
-        // 计算重新绘制图片的尺寸
-        Rectangle rectangle = calculatorRotatedSize(imageWidth, imageHeight, angle);
-
         // 获取图片透明度
         int transparency = bufferedImage.getColorModel().getTransparency();
 
         // 绘制验证码图片
         BufferedImage captcha = null;
-        captcha = new BufferedImage(rectangle.width, rectangle.height, transparency);
-        Graphics2D graphics = captcha.createGraphics();
         // 平移位置
-        graphics.translate((rectangle.width - imageWidth) / 2, (rectangle.height - imageHeight) / 2);
+        captcha = new BufferedImage(imageWidth, imageHeight, transparency);
+        Graphics2D graphics = captcha.createGraphics();
+        graphics.translate(0, 0);
         // 旋转角度
         graphics.rotate(Math.toRadians(angle), (double) imageWidth / 2, (double) imageHeight / 2);
         // 绘图
@@ -71,30 +68,6 @@ public class RotateCaptcha extends Configurable implements Producer {
     public boolean verify(int angle, int inputAngle) {
         angle = angle+getConfig().getRotationAngleOffset();
         return (inputAngle >= angle && inputAngle <= angle+getConfig().getRotationFault())
-                        || (inputAngle <= angle &&  inputAngle >= angle-getConfig().getRotationFault());
-    }
-
-    /**
-     * 计算旋转后的尺寸
-     * @param width 宽度
-     * @param height 高度
-     * @param angel 角度
-     * @return 尺寸
-     */
-    private static Rectangle calculatorRotatedSize(int width, int height, int angel) {
-        if (angel >= 90) {
-            angel = angel % 90;
-        }
-        double r = Math.sqrt(height * height + width * width) / 2;
-        double len = 2 * Math.sin(Math.toRadians(angel) / 2) * r;
-        double angel_alpha = (Math.PI - Math.toRadians(angel)) / 2;
-        double angel_dalta_width = Math.atan((double) height / width);
-        double angel_dalta_height = Math.atan((double) width / height);
-
-        int len_dalta_width = (int) (len * Math.cos(Math.PI - angel_alpha - angel_dalta_width));
-        int len_dalta_height = (int) (len * Math.cos(Math.PI - angel_alpha - angel_dalta_height));
-        int des_width = width + len_dalta_width * 2;
-        int des_height = height + len_dalta_height * 2;
-        return new Rectangle(new Dimension(des_width, des_height));
+                || (inputAngle <= angle &&  inputAngle >= angle-getConfig().getRotationFault());
     }
 }
